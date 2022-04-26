878973e81737df14796a22ae6cf0ffbefe387f40
lizhongxiu <58903744+lizhongxiu@users.noreply.github.com>
2022-04-26 16:32:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Vite + Vue3 + Typescript + Pinia + Vueuse
+
+一个大厂企业级前端项目
+
+## 通过这个项目你可以学到
+
+-   如何使用使用 Vite 搭建项目
+-   如何在 Vite 中集成 `typescript`
+-   如何在 Vite 中集成 `vue-router4` 和 `pinia`
+-   如何使用 vue3 的伴侣 `vueuse`
+-   如何在项目中集成 `eslint` 和 `prettier` 保证代码质量
+-   如何规范化 `git` 提交信息
+-   如何为团队开发`专属的项目模板`
+
+> <font style="color:red">如果觉得项目模板不错的话，欢迎 Star 支持就好，感谢大家！</font>
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-config-provider :locale="locale">
+        <router-view></router-view>
+    </el-config-provider>
+</template>
+<script lang="ts" setup>
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+const locale = zhCn;
+</script>
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
